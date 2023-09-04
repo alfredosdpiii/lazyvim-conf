d966f1babf957edf7225d5a422a150459069a994
alfredosdpiii <alfredo.sdpiii@gmail.com>
2023-09-04 23:13:00 +0800

    changed to lazy true

--- lua/plugins/all.lua
+++ lua/plugins/all.lua
@@ -31,7 +31,7 @@ return {
   },
   {
     "MeF0504/vim-pets",
-    event = "VeryLazy",
+    lazy = true,
     config = function()
       vim.g.pets_default_pet = "cat"
       vim.g.pets_lifetime_enable = 0

a790c24f96e91863e5532b6649880b86e738ae1a
alfredosdpiii <alfredo.sdpiii@gmail.com>
2023-09-05 16:23:04 +0800

    config for prettier

--- lua/plugins/all.lua
+++ lua/plugins/all.lua
@@ -58,4 +58,17 @@ return {
       vim.g.lazygit_floating_window_scaling_factor = 1
     end,
   },
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      table.insert(opts.ensure_installed, "prettierd")
+    end,
+  },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    opts = function(_, opts)
+      local nls = require("null-ls")
+      table.insert(opts.sources, nls.builtins.formatting.prettierd)
+    end,
+  },
 }

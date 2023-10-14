290266be78ecdb1b949c21c7df3e266bcaf7e388
alfredosdpiii <alfredo.sdpiii@gmail.com>
2023-10-14 19:17:55 +0800

    fixed conf

--- lua/plugins/all.lua
+++ lua/plugins/all.lua
@@ -66,13 +66,6 @@ return {
     end,
   },
   {
-    "jose-elias-alvarez/null-ls.nvim",
-    opts = function(_, opts)
-      local nls = require("null-ls")
-      table.insert(opts.sources, nls.builtins.formatting.prettierd)
-    end,
-  },
-  {
     "ThePrimeagen/harpoon",
     lazy = false,
     dependencies = {
